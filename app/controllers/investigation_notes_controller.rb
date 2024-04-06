2fb69ea0ec7679da2d1851f0df7f1b81835dc882
rosyyang224 <rey@andrew.cmu.edu>
2024-04-05 21:01:47 -0400

    fixed investigation notes except for the count by 1 stuff

--- app/controllers/investigation_notes_controller.rb
+++ app/controllers/investigation_notes_controller.rb
@@ -1,29 +1,23 @@
 class InvestigationNotesController < ApplicationController
-    before_action :authenticate_officer!
-    before_action :set_investigation
     before_action :set_investigation_note, only: [:show, :edit, :update, :destroy]
   
     def new
-      @investigation_note = @investigation.investigation_notes.new
+        @investigation_note = InvestigationNote.new
+        @investigation = Investigation.find(params[:investigation_id])
     end
   
     def create
-      @investigation_note = @investigation.investigation_notes.new(investigation_note_params)
-      @investigation_note.officer = current_officer
-  
+        @investigation_note = InvestigationNote.new(investigation_note_params)
+
       if @investigation_note.save
         flash[:notice] = "Successfully added investigation note."
         redirect_to investigation_path(@investigation_note.investigation)
       else
-        render :new, locals: { investigation: @investigation }
+        render action: 'new', locals: { investigation: @investigation }
       end
     end
   
     private
-  
-    def set_investigation
-      @investigation = Investigation.find(params[:investigation_id])
-    end
   
     def set_investigation_note
       @investigation_note = InvestigationNote.find(params[:id])

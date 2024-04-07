3bcbe163f42640c1b645a0dbbad5b7ae9b00e45e
rosyyang224 <rey@andrew.cmu.edu>
2024-04-07 18:04:05 -0400

    we're working on it lol

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,17 +6,18 @@ class SessionsController < ApplicationController
         officer = Officer.find_by_username(params[:username])
         if officer && officer.authenticate(params[:password])
             session[:officer_id] = officer.id
-            flash[:notice] = "Officer is logged in."
-            redirect_to home_path
+            # flash[:notice] = "Officer is logged in."
+            redirect_to home_path, notice: "Logged in!"
         else
-            flash.now[:alert] = "Invalid username or password"
+            # flash.now[:alert] = "Invalid username or password"
+            flash.now.alert = "Username and/or password is invalid"
             render action: 'new'
         end
     end
 
     def destroy
         session[:officer_id] = nil
-        flash[:notice] = "Logged out!"
-        redirect_to home_path
+        # flash[:notice] = "Logged out!"
+        redirect_to home_path, notice: "Logged out!"
     end
 end

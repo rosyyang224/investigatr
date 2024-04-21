6631cd5d1112b60cda70d5227078d62cd3bea840
rosyyang224 <rey@andrew.cmu.edu>
2024-04-21 02:52:08 -0400

    changed assignments but date isnt owkring

--- app/javascript/components/Assignment.js
+++ app/javascript/components/Assignment.js
@@ -1,93 +1,31 @@
-// import React from "react";
-// import FormattedDate from "./FormattedDate";
-
-// function Assignment({ investigation }) {
-//     const { current_assignments: assignments } = investigation.data.attributes;
-
-//     const renderAssignments = () => {
-//         if (assignments.length === 0) {
-//             return (
-//                 <p>There are no officers currently assigned to this investigation.</p>
-//             );
-//         }
-
-//         return (
-//             <ul>
-//                 {assignments.map((assignment) => (
-//                     <li key={`assignment-${assignment.data.id}`}>
-//                         <p>
-//                             <b>Officer:</b> {assignment.data.attributes.officer.data.attributes.rank} {assignment.data.attributes.officer.data.attributes.first_name} {assignment.data.attributes.officer.data.attributes.last_name} (as of:{" "}
-//                             <FormattedDate date={assignment.data.attributes.start_date} />)
-//                         </p>
-//                     </li>
-//                 ))}
-//             </ul>
-//         );
-//     };
-
-//     return (
-//         <div className="card yellow lighten-5">
-//             <div className="card-content">
-//                 <span className="card-title">Current Assignments</span>
-//                 {renderAssignments()}
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default Assignment;
-
 import React from "react";
 import FormattedDate from "./FormattedDate";
 
-// Current Assignments: 
-// There will be a card/panel showing the assignment of officers who are currently working on the investigation. 
-// This list will have the rank and name of the officer as well as the date the officer was assigned to the case. 
-// If there are no officers currently assigned to the investigation, a note to that effect should appear. 
-
 function Assignment({ investigation }) {
-    const investigationData = investigation.data.attributes;
-    const assignments = investigationData.current_assignments;
-    console.log(investigation);
-    var officer_first_names = [];
-    var officer_last_names = [];
-    var officer_ranks = [];
-    var start_dates = [];
-    assignments.forEach(assignment => {
-        officer_first_names.push(assignment.data.attributes.officer.data.attributes.first_name);
-        officer_last_names.push(assignment.data.attributes.officer.data.attributes.last_name);
-        officer_ranks.push(assignment.data.attributes.officer.data.attributes.rank);
-        start_dates.push(assignment.data.attributes.start_date);
-    });
+    const { current_assignments } = investigation.data.attributes;
 
     return (
-        <>
-            <div class="card yellow lighten-5">
-                <div class="card-content">
-                    <span class="card-title">Current Assignments</span>
-                    {assignments.length === 0 ? (
-                        <p>There are no officers currently assigned to this investigation.</p>
-                    ) : (
-                        <>
-                            <ul>
-                                {assignments.map((assignment, index) => {
-                                    return (
-                                        <li key={`assignment-${assignment.data.id}`}>
-                                            <p>
-                                                <ul>
-                                                    <li>&bull; {officer_ranks[index]} {officer_first_names[index]} {officer_last_names[index]} (as of: {FormattedDate(start_dates[index])})</li>
-                                                </ul>
-                                            </p>
-                                        </li>
-                                    );
-                                })}
-                            </ul>
-                        </>
-                    )}
-
-                </div>
+        <div className="card yellow lighten-5">
+            <div className="card-content">
+                <span className="card-title">Current Assignments</span>
+                {current_assignments.length === 0 ? (
+                    <p>There are no officers currently assigned to this investigation.</p>
+                ) : (
+                    <ul>
+                        {current_assignments.map((assignment) => {
+                            const officer = assignment.data.attributes.officer.data.attributes;
+                            return (
+                                <li key={`assignment-${assignment.data.id}`}>
+                                    <p>
+                                        &bull; {officer.rank} {officer.first_name} {officer.last_name} (as of: <FormattedDate dateString={assignment.data.attributes.start_date} />)
+                                    </p>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                )}
             </div>
-        </>
+        </div>
     );
 }
 
